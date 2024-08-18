f40e79e4c9d6d6cdc09ebe04bf1198c4eeb2d14a
Sai Charan <42805601+saicharanakula@users.noreply.github.com>
2024-08-18 22:57:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-00aa03f67b64f75dd" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
-
+  key_name      = "devops"
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
